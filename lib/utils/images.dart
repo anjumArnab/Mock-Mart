456005d77e7cbcee5258ca9ee1cbdc3a6cf79b9c
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2026-01-01 00:32:15 +0600

    image format extension for asset images

---
+++ lib/utils/images.dart
@@ -0,0 +1,11 @@
+class Images {
+  static String get banner => 'banner'.png;
+  static String get banner01 => 'banner01'.png;
+  static String get storeBanner => 'store_banner'.png;
+  static String get bigSale => 'big_sale'.png;
+  static String get productImg => 'product_img'.png;
+}
+
+extension on String {
+  String get png => 'assets/images/$this.png';
+}
