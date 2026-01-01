a298a88af6b242a955445c347375ed9bb510b4cf
anjumArnab <anjumarnab050@gmail.com>
2026-01-01 14:10:18 +0600

    necessary app constansts

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,9 @@
+import '../common/models/language_model.dart';
+
+class AppConstants {
+
+  static List<LanguageModel> languages = [
+    LanguageModel(languageName: 'English', languageCode: 'en'),
+    LanguageModel(languageName: 'বাংলা', languageCode: 'bn'),
+  ];
+}
