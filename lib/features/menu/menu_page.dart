263cbd734a89a83783c0c22b177cd8ef7ca1ec28
anjumArnab <anjumarnab050@gmail.com>
2026-01-01 16:51:24 +0600

    DropDownButton added to change language

--- lib/features/menu/menu_page.dart
+++ lib/features/menu/menu_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mock_mart/theme/app_theme.dart';
+import 'package:mock_mart/utils/dimensions.dart';
+import '../../utils/app_constants.dart';
+import '../../utils/gaps.dart';
+import '../language/controllers/language_controller.dart';
 
 class MenuPage extends StatelessWidget {
   final VoidCallback onThemeToggle;
@@ -12,24 +17,22 @@ class MenuPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Menu'),
+        title: Text("menu".tr),
         backgroundColor: AppTheme.primaryColor,
       ),
       body: ListView(
         children: [
           SwitchListTile(
             title: Text(
-              'Dark Mode',
+              "dark_mode".tr,
               style: TextStyle(
                 fontSize: 16,
                 color: AppTheme.getTextColor(context),
               ),
             ),
             subtitle: Text(
-              isDark ? 'Enabled' : 'Disabled',
-              style: TextStyle(
-                color: AppTheme.getSecondaryTextColor(context),
-              ),
+              isDark ? "enabled".tr : "disabled".tr,
+              style: TextStyle(color: AppTheme.getSecondaryTextColor(context)),
             ),
             value: isDark,
             onChanged: (value) {
@@ -40,7 +43,27 @@ class MenuPage extends StatelessWidget {
               color: AppTheme.primaryColor,
             ),
           ),
-
+          Row(
+            children: [
+              Gaps.horizontalGapOf(Dimensions.paddingSizeDefault),
+              Icon(Icons.translate),
+              Gaps.horizontalGapOf(Dimensions.paddingSizeDefault),
+              DropdownButton<String>(
+                value: Get.find<LanguageController>().locale.languageCode,
+                items: AppConstants.languages.map((lang) {
+                  return DropdownMenuItem(
+                    value: lang.languageCode,
+                    child: Text(lang.languageName!),
+                  );
+                }).toList(),
+                onChanged: (String? code) {
+                  if (code != null) {
+                    Get.find<LanguageController>().setLanguage(Locale(code));
+                  }
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
