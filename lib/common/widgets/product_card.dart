2ffdebb316c26ca4f96f8168e207acf9869ce566
anjumArnab <anjumarnab050@gmail.com>
2026-01-07 16:27:38 +0600

    animation added on the favourite icon

--- lib/common/widgets/product_card.dart
+++ lib/common/widgets/product_card.dart
@@ -4,8 +4,7 @@ import 'package:mock_mart/utils/dimensions.dart';
 import 'package:mock_mart/utils/images.dart';
 import 'package:mock_mart/utils/text_styles.dart';
 
-
-class ProductCard extends StatelessWidget {
+class ProductCard extends StatefulWidget {
   final String title;
   final String price;
   final String oldPrice;
@@ -20,7 +19,79 @@ class ProductCard extends StatelessWidget {
   });
 
   @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard>
+    with SingleTickerProviderStateMixin {
+
+  late final AnimationController controller;
+  late final Animation<double> scaleAnimation;
+  late final Animation<double> opacityAnimation;
+
+  bool isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 700),
+    );
+
+    scaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(begin: 1.0, end: 1.5).chain(
+          CurveTween(curve: Curves.easeOutBack),
+        ),
+        weight: 30,
+      ),
+      TweenSequenceItem(
+        tween: Tween(begin: 1.5, end: 1.0).chain(
+          CurveTween(curve: Curves.easeIn),
+        ),
+        weight: 30,
+      ),
+    ]).animate(controller);
+
+    opacityAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(begin: 1.0, end: 0.3),
+        weight: 30,
+      ),
+      TweenSequenceItem(
+        tween: Tween(begin: 0.3, end: 1.0),
+        weight: 30,
+      ),
+    ]).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  void onIconTap() {
+    if (controller.isAnimating) return;
+
+    setState(() {
+      isLiked = !isLiked;
+    });
+
+    controller.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).extension<CustomThemeColors>()!;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -29,78 +100,106 @@ class ProductCard extends StatelessWidget {
             Container(
               height: Dimensions.productCardImageHeight,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(Dimensions.productCardBorderRadius),
+                borderRadius: BorderRadius.circular(
+                  Dimensions.productCardBorderRadius,
                 ),
               ),
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(Dimensions.productCardBorderRadius),
+                borderRadius: BorderRadius.circular(
+                  Dimensions.productCardBorderRadius,
+                ),
                 child: Image.asset(
                   Images.productImg,
                   fit: BoxFit.cover,
                 ),
               ),
             ),
+
             Positioned(
               bottom: Dimensions.productCardPadding,
               right: Dimensions.productCardPadding,
-              child: Container(
-                padding: EdgeInsets.all(Dimensions.productCardFavoriteIconPadding),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).extension<CustomThemeColors>()!.cardColor,
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(
-                  Icons.favorite_border,
-                  size: Dimensions.productCardFavoriteIconSize,
-                  color: Theme.of(context).extension<CustomThemeColors>()!.negativeColor,
+              child: GestureDetector(
+                onTap: onIconTap,
+                child: AnimatedBuilder(
+                  animation: controller,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: scaleAnimation.value,
+                      child: Opacity(
+                        opacity: opacityAnimation.value,
+                        child: child,
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(
+                      Dimensions.productCardFavoriteIconPadding,
+                    ),
+                    decoration: BoxDecoration(
+                      color: theme.cardColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      isLiked
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                      size: Dimensions.productCardFavoriteIconSize,
+                      color: theme.negativeColor,
+                    ),
+                  ),
                 ),
               ),
             ),
           ],
         ),
+
         Padding(
           padding: EdgeInsets.all(Dimensions.productCardPadding),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                title,
+                widget.title,
                 style: productTitleTextStyle.copyWith(
-                  color: Theme.of(context).extension<CustomThemeColors>()!.textColor,
+                  color: theme.textColor,
                 ),
                 maxLines: 1,
                 overflow: TextOverflow.ellipsis,
               ),
               SizedBox(height: Dimensions.productCardSpacing),
               Text(
-                price,
+                widget.price,
                 style: productPriceTextStyle.copyWith(
-                  color: Theme.of(context).extension<CustomThemeColors>()!.textColor,
+                  color: theme.textColor,
                 ),
               ),
               Row(
                 children: [
                   Text(
-                    oldPrice,
+                    widget.oldPrice,
                     style: discountedPriceTextStyle.copyWith(
-                      color: Theme.of(context).extension<CustomThemeColors>()!.greyTextColor,
+                      color: theme.greyTextColor,
                     ),
                   ),
                   SizedBox(width: Dimensions.spacingSmall),
                   Container(
                     padding: EdgeInsets.symmetric(
-                      horizontal: Dimensions.discountBadgePaddingHorizontal,
-                      vertical: Dimensions.discountBadgePaddingVertical,
+                      horizontal:
+                      Dimensions.discountBadgePaddingHorizontal,
+                      vertical:
+                      Dimensions.discountBadgePaddingVertical,
                     ),
                     decoration: BoxDecoration(
-                      color: Theme.of(context).extension<CustomThemeColors>()!.negativeColor,
-                      borderRadius: BorderRadius.circular(Dimensions.discountBadgeBorderRadius),
+                      color: theme.negativeColor,
+                      borderRadius: BorderRadius.circular(
+                        Dimensions.discountBadgeBorderRadius,
+                      ),
                     ),
                     child: Text(
-                      discount,
+                      widget.discount,
                       style: discountPercentageTextStyle.copyWith(
-                        color: Theme.of(context).colorScheme.error,
+                        color:
+                        Theme.of(context).colorScheme.error,
                       ),
                     ),
                   ),
